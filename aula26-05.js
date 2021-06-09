84cce74da176b2899cb50247e4838ce162be3169
EstherPimentel <stherpimentel@yahoo.com.br>
2021-06-09 01:08:03 -0300

    colocando exercícios no github

---
+++ aula26-05.js
@@ -0,0 +1,10 @@
+let nome = "Esther Porto";
+let idade = 33;
+let exercicosFeitos = false;
+
+console.log("Meu nome é: " + nome);
+console.log("Minha idade é: " + idade + " anos");
+console.log("Fiz os exercícios hoje: " + false)
+
+nome = "Joana";
+console.log(nome);
---
+++ exercício6.js
@@ -0,0 +1,13 @@
+/*let mes = "dezembro"
+console.log(mes == "dezembro" ? "FELIZ NATAL!" : "MORRA")*/
+
+let mes = "abril"
+switch(mes){
+    case "janeiro" : console.log("Feliz Ano Novo")
+    break;
+    case "fevereiro" : console.log("Feliz Carnaval")
+    break;
+    case "dezembro" : console.log("Feliz Natal")
+    break;
+    default : console.log("escolher um mes válido")
+}
